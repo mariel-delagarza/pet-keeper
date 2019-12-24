7d0e52ed24078345e1fd96add4560b79198c49fa
mariel-delagarza <mariel.code@gmail.com>
2019-12-24 12:25:58 -0600

    Add configure development block

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do 
+  set :database, 'sqlite3:db/database.db'
+end 
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
@@ -10,3 +14,4 @@ ActiveRecord::Base.establish_connection(
 
 require './app/controllers/application_controller'
 require_all 'app'
+
