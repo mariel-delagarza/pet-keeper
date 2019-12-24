3d8ff013e9b8aad9e07377711c6556f1faf3904d
mariel-delagarza <mariel.code@gmail.com>
2019-12-24 12:00:12 -0600

    Aded users_controller - for create, login, logout functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  get '/users/create' do
+    erb :'/users/create'
+  end
+
+  post '/users' do
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect to '/pets'
+      else
+        redirect to '/users/create'
+      end
+    end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/pets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+end
